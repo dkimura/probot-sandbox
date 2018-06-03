94433f912270f2eac77e42555955a0705a19b32b
Daichi Kimura <dkimura@bdd.xyz>
2018-06-03 15:58:45 +0900

    feat: Improve method

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+const approvePullRequest = async context => {
+  context.log("Received event");
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  const params = context.issue({ event: "APPROVE" });
+  context.log(params);
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  return context.github.pullRequests.createReview(params);
+};
 
-  module.exports = robot => {
-    robot.on("pull_request.opened", async context => {
-      console.log("Received event");
-      // `context` extracts information from the event, which can be passed to
-      // GitHub API calls. This will return:
-      //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-      const params = context.issue({ event: "APPROVE" });
-      console.log(params);
-
-      // Post a comment on the issue
-      return context.github.pull_request.createReview(params);
-    });
-  };
-}
+module.exports = robot => {
+  robot.on(
+    ["pull_request.opened", "pull_request.reopened"],
+    approvePullRequest
+  );
+};
